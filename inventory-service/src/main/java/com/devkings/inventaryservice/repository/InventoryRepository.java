069228b2991e1417a6724b16b84eaa89a039c886
Shivam Baghel <shivambghl2000@gmail.com>
2024-05-22 17:23:32 +0530

    Order and inventory service is connected using webClient

--- inventory-service/src/main/java/com/devkings/inventaryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/devkings/inventaryservice/repository/InventoryRepository.java
@@ -3,8 +3,9 @@ package com.devkings.inventaryservice.repository;
 import com.devkings.inventaryservice.model.Inventary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventary,Long> {
-    Optional<Inventary> findBySkuCode(String skuCode);
+    List<Inventary> findBySkuCodeIn(List<String> skuCode);
 }
--- order-service/src/main/java/com/devkings/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/devkings/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.devkings.orderservice.service;
 
+import com.devkings.orderservice.dto.InventoryResponse;
 import com.devkings.orderservice.dto.OrderLineItemsdto;
 import com.devkings.orderservice.dto.OrderRequest;
 import com.devkings.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.devkings.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,7 +21,9 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    public void placeOrder(OrderRequest orderRequest){
+    private final WebClient webClient;
+
+    public void placeOrder(OrderRequest orderRequest) throws IllegalAccessException {
         Order order=new Order();
         order.setOrder_number(UUID.randomUUID().toString());
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsdtos()
@@ -26,7 +31,26 @@ public class OrderService {
                 .map(this::maptoDto)
                 .toList();
         order.setOrderLineItemsListl(orderLineItems);
-        orderRepository.save(order);
+
+        List<String> skuCodes = order.getOrderLineItemsListl().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+//        Call inventory service to check the presence of the item
+        InventoryResponse[] inventoryResponseArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        assert inventoryResponseArray != null;
+        boolean allProductInStock =Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+        if(allProductInStock){
+            orderRepository.save(order);
+        }else{
+            throw new IllegalAccessException("Product is not in stock , please try again later.");
+        }
     }
 
     private OrderLineItems maptoDto(OrderLineItemsdto orderLineItemsdto) {
